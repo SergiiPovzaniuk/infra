4b813deaf8712d46e595f3fe658ffe16c17a0cb9
Sergii Povzaniuk <sergii.povzaniuk@gmail.com>
2026-08-01 14:41:03 +0200

    feat(terraform): add network, security-group, ec2 modules and prod env

---
+++ terraform/modules/ec2/outputs.tf
@@ -0,0 +1,7 @@
+output "public_ip" {
+  value = try(aws_eip.this[0].public_ip, aws_instance.this.public_ip)
+}
+
+output "public_dns" {
+  value = aws_instance.this.public_dns
+}
---
+++ terraform/modules/security-group/variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "ingress_rules" {
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = optional(list(string), [])
+    self        = optional(bool, false)
+  }))
+}
